b7c4445b70940967a3e161c86a0245b81ba98485
yosiinoya <yoshuajulianosubianto@gmail.com>
2026-06-17 00:46:08 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something For You cal`.split('')
+const text = `A Little Gift For You Cal`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
